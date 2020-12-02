4b494038e6ae21bd1bcdfa50b11bf28ac85fe642
tdpaul315@gmail.com <tiaunaj.tsg@gmail.com>
2020-12-02 15:51:16 -0500

    commit tree ab6d9258d5f32715801081569bd1326d00870ce2

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+
+
+function writeCards(names, e) {
+    let arr = []
+    for (let n = 0; n < names.length; n++){
+    arr.push(`Thank you, ${names[n]}, for the wonderful ${e} gift!`)}
+        return arr
+}
+
+writeCards(names);
+
